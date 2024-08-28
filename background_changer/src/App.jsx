2044230305790009762a96cd61ab5ab8e64d318d
Svapnil16 <svapnilrathod16@gmail.com>
2024-08-28 22:36:55 +0530

    upadte app.jsx

--- background_changer/src/App.jsx
+++ background_changer/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   let [color, setColor ] = useState("cyan");
 
   return (
-    <div className="w-full h-screen duration-200 "
+    <div className="w-full h-screen duration-100"
     style={{backgroundColor :color}}>
       <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2 ">
         <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white rounded-lg px-3 py-2 ">
